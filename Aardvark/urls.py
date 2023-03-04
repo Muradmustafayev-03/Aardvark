8771c764ccf1f29be316ba355bfda5079e1cac9b
Murad <muradmustafayev03@gmail.com>
2023-03-04 14:22:24 +0400

    urls created

--- Aardvark/urls.py
+++ Aardvark/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/', include('rest_framework.urls')),
+    path('api/v1/docs/', include_docs_urls('Azercell')),
+    path('api/v1/', include('db_management.urls')),
 ]

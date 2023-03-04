80816f042a14ff2a7fa1318d2811b3fe3cd67df2
Murad <muradmustafayev03@gmail.com>
2023-03-04 14:05:28 +0400

    models registered to the admin panel

--- db_management/admin.py
+++ db_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+post_models = apps.get_app_config('db_management').get_models()
+
+for model in post_models:
+    admin.site.register(model)
